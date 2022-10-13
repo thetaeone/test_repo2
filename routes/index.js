e565ba77518ed902944aa4c11872603be13b29ae
kimandkim <115604221+kimandkim@users.noreply.github.com>
2022-10-13 15:18:22 +0900

    Edit index.js

--- routes/index.js
+++ routes/index.js
@@ -3,7 +3,7 @@ var router = express.Router();
 
 /* GET home page. */
 router.get('/', function(req, res, next) {
-  res.render('index', { title: '김민재' });
+  res.render('index', { title: '민재' });
 });
 
 module.exports = router;
